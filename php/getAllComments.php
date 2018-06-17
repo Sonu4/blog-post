930994a31ef0f641f427927833fab39e1b915d05
sonu4 <kotheshubham4@gmail.com>
2018-06-17 22:50:19 +0530

    Project Completed

---
+++ php/getAllComments.php
@@ -0,0 +1,18 @@
+<?php 
+		require_once 'config.php';
+
+		$sql="select b.blog_name,c.*,p.name from blogs as b inner join comments as c on c.blog_id=b.id_b 
+        inner join profile as p on p.id=c.blog_of where c.status=1";
+
+		$result=mysqli_query($con,$sql);
+
+		
+		while ( $row=mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+			$success[]=$row;
+		}
+
+		echo json_encode($success);
+
+		exit();
+
+ ?>
